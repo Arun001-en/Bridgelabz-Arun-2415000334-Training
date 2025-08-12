340a0eb399b72e856a553002b6fb3556f620895c
arun pundhir <arunpundhir2021@gmail.com>
2025-08-12 14:34:30 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,119 @@
+// Sum of Two Number
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the first number:");
+//         int n1 = sc.nextInt();
+//         System.out.print("Enter the second number:");
+//         int n2 = sc.nextInt();
+//         int sum = n1+n2;
+//         System.out.println("The sum of two number is:"+sum);
+
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the Celsius:");
+//         double c = sc.nextdouble();
+//         double f = (c*9/5)+32;
+//         System.out.println(f);
+        
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the radius is:");
+//        double r = sc.nextDouble();
+//        double area = Math.PI*r*r;
+//        System.out.println(area);
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the radius:");
+//         double radius = sc.nextDouble();
+//         System.out.println("Enter the height:");
+//         double height = sc.nextDouble();
+//         double volume = Math.PI*radius*radius*height;
+//         System.out.println(volume);
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the p:");
+//         float p = sc.nextFloat();
+//         System.out.println("Enter the Rate:");
+//         float r = sc.nextFloat();
+//         System.out.println("Enter the Time:");
+//         float t = sc.nextFloat();
+//         float Si = (p*r*t)/100;
+//         System.out.println(Si);
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the length:");
+//         float l = sc.nextFloat();
+//         System.out.println("Enter the width:");
+//         float w = sc.nextFloat();
+//         float perimeter = 2*(l+w);
+//         System.out.println(perimeter);
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter first number:");
+//         float a = sc.nextFloat();
+//         System.out.println("Enter Second number:");
+//         float b = sc.nextFloat();
+//         System.out.println("Enter Third number:");
+//         float c = sc.nextFloat();
+//         float average = (a+b+c)/3;
+//         System.out.println(average);
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class FirstProgram {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the Kilometer:");
+//         float k = sc.nextFloat();
+//         float miles = k*0.621371;
+//         System.out.println(miles);
+//     }
+// }
+
+
+
