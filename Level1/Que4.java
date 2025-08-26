c5d1c5edba8b36cea820477bb683077da20e5749
Arun001-en <arunpundhir2021@gmail.com>
2025-08-26 13:39:59 +0530

    Add files via upload

---
+++ Level1/Que4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Que4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int natural = n*(n+1)/2;
+        System.out.println(natural);
+    }
+}
